aeabe87a43fdd00a08546a770db834ec4c8b601d
srib <srib@users.noreply.github.com>
2014-09-19 01:09:34 -0500

    Initial creation of FetchBookFromDLI script
    Script to fetch book from DLI website and convert it to PDF.

---
+++ FetchBookFromDLI.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+'''Fetches TIFF files from the DLI website and converts them to PDF'''
+
+import sys
+
+def ParseCommandLineArgs():
+    print "Not yet implemented"
+
+def FetchTiffFiles():
+    print "Not yet implemented"
+
+def ConvertToPDF():
+    print "Not yet implemented"
+
+def main():
+    ParseCommandLineArgs();
+    FetchTiffFiles();
+    ConvertToPDF();
+
+
+if __name__=="__main__":
+    print 'For now do nothing'
+    main();
+
+
